ebbf6b5453d059c1181821547653ae3d0af82141
Emilis Vinkevicius <vinkeemilis@gmail.com>
2026-02-25 15:36:46 +0200

    Pridėjau ,,play" mygtuką

---
+++ src/js/control.js
@@ -0,0 +1,11 @@
+const player = videojs('player');
+
+player.src({
+  src: 'https://d2zihajmogu5jn.cloudfront.net/bipbop-advanced/bipbop_16x9_variant.m3u8',
+  type: 'application/x-mpegURL'
+});
+
+document.getElementById('btnPlay').addEventListener('click', () => {
+  player.play();
+});
+
